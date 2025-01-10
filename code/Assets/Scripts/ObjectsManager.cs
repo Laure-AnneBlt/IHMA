575dc7cb20e55700451052a89282c7c8fd171d68
ABMesbh <mesbahabde@gmail.com>
2025-01-10 14:01:58 +0100

    State 0: black screen fixed

--- code/Assets/Scripts/ObjectsManager.cs
+++ code/Assets/Scripts/ObjectsManager.cs
@@ -25,12 +25,12 @@ public class DistributeInLine : MonoBehaviour
             // First set of objects
             Vector3 newPosition = table.position + new Vector3(-table.localScale.x / 2, 0.4f, table.localScale.z / 2) + new Vector3(i * spacing, 0, 0);
             GameObject newObject = Instantiate(prefabs[i], newPosition, Quaternion.identity);
-            newObject.AddComponent<Draggable>();
+            newObject.AddComponent<DragObject>();
             objects.Add(newObject);
             // Second set of objects
             Vector3 newPosition2 = table.position + new Vector3(-table.localScale.x / 2, 0.4f, -table.localScale.z / 2) + new Vector3(i * spacing, 0, 0);
             GameObject newObject2 = Instantiate(prefabs[i], newPosition2, Quaternion.Euler(0, 90, 0));
-            newObject2.AddComponent<Draggable>();
+            newObject2.AddComponent<DragObject>();
             objects.Add(newObject2);
 
         }
@@ -130,7 +130,7 @@ public class DistributeInLine : MonoBehaviour
     }
 }
 
-using UnityEngine;
+
 
 public class DragObject : MonoBehaviour
 {
